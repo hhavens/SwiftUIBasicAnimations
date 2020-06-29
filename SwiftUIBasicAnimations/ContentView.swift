2d5e973080917d58364d596565efed140c1422a9
Henry Havens <hhavens@hotmail.com>
2020-06-29 09:37:16 -0500

    Initial Commit

---
+++ SwiftUIBasicAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBasicAnimations
+//
+//  Created by Henry Havens on 6/29/20.
+//  Copyright © 2020 Henry Havens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
